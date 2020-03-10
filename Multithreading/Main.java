27c6aeebf91a35794ff10f3537cc3f583addd71c
McSohan <60097620+McSohan@users.noreply.github.com>
2020-03-10 13:25:03 +0530

    Create Main.java

---
+++ Multithreading/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Market 0 = new Market(4);
+        Customer c1 = new Customer(1, market);
+        Customer ob2 = new Customer(2, market);
+        Farmer obj3 = new Farmer (market, 1);
+        Farmer obj4 = new Farmer (market, 2);
+        
+    }
+}
